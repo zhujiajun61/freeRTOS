df21187fea1cb67cb5c604bc0fffe4a093006956
zhujiajun61 <1615901203@qq.com>
2025-07-03 16:51:19 +0800

    delete

--- Project/Objects/list.d
+++
@@ -1,8 +0,0 @@
-.\objects\list.o: ..\freertos\list.c
-.\objects\list.o: ..\freertos\include\list.h
-.\objects\list.o: ..\freertos\include\portmacro.h
-.\objects\list.o: D:\ruanjian\Keil\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\list.o: D:\ruanjian\Keil\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-.\objects\list.o: D:\ruanjian\Keil\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\objects\list.o: ..\freertos\include\FreeRTOSConfig.h
-.\objects\list.o: ..\freertos\include\portable.h
